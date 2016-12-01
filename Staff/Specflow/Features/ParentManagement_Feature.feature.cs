962659b53f05fe7fe228246390fc6c0f6109bc6b
daretodreamz <bing.wang2016@hotmail.com>
2016-12-01 17:09:19 +1300

    move Specflow files to single folder

--- Staff/Specflow/Features/ParentManagement_Feature.feature.cs
+++ Staff/Specflow/Features/ParentManagement_Feature.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace AimyTest.Features
+namespace AimyTest.Specflow.Features
 {
     using TechTalk.SpecFlow;
     
